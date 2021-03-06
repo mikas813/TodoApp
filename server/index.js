84613e31bea6ea0e3b4987e6fa9c8ed45f591d7b
Mikael Lasichuk <mikaellasichuk@MacBook-Pro-Mikael.local>
2021-03-06 15:57:18 +0000

    created a new DB and connected to the server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = config.get('port');
 
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
+const connect = async () => {
+	try {
+		await mongoose.connect(config.get('mongo_db_connection_string'), {
+			useCreateIndex: true,
+			useUnifiedTopology: true,
+			useNewUrlParser: true
+		});
+		app.listen(PORT, () => {
+			console.log(`Server started on port: ${PORT} and have been successfully connected to the DB`)
+		});
+	} catch (e) {
+		console.log(e.message);
+		process.exit(1)
+	}
+};
+
+connect();
+
